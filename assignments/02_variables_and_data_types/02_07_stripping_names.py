97b076014a128652367808875c31642f95ea4333
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-07 13:00:40 -0400

    Lstrip,rstrip

---
+++ assignments/02_variables_and_data_types/02_07_stripping_names.py
@@ -0,0 +1,6 @@
+#
+quote ="        \n\n\n\n     Got a shift at 10 a.m. (yeah), gotta dip at 10 p.m. (nah) \t\t\t\t\n" 
+print(quote)
+print(quote.strip())
+print(quote.lstrip())
+print(quote.rstrip())

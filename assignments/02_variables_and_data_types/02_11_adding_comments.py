1541bdeaae2c6fca5bfa7a83a4901e290cda3842
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-07 15:18:29 -0400

    adding commets

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,7 @@
+# adding comments 
+# Ciani Elyse Mason
+#September 7, 2026
+#This program displays my favorite in a message 
+favorite_number = 7
+message = f"My favorite number is {favorite_number}"
+print(message)

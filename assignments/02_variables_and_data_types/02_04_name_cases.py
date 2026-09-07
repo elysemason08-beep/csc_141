553351869a82ebfeaa72bbe8052b9672c592a041
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-07 12:46:33 -0400

    ciani is upper

---
+++ assignments/02_variables_and_data_types/02_04_name_cases.py
@@ -0,0 +1,5 @@
+# name =ciani elyse mason 
+name = "ciani is the queen of python" 
+print(name.title())
+print(name.upper())
+print(name.lower())
---
+++ assignments/02_variables_and_data_types/02_05_famous_quote.py
@@ -0,0 +1,5 @@
+# famous quote 
+quote =  "When we drive away in secret you become a volenteer so dont scream when we take yout the world is quite here"
+person = "Lemoney snicket"
+
+print(person + " once said," + quote)

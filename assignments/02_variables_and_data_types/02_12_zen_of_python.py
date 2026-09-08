2d15ae2bedc68605f2f1b536bb4724e89631a644
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-08 13:38:19 -0400

    there is beauty in complexcity

--- assignments/02_variables_and_data_types/02_12_zen_of_python.py
+++ assignments/02_variables_and_data_types/02_12_zen_of_python.py
@@ -1,2 +1,9 @@
 # the zen of python
-import this 
+import this
+phrase ="Complex is better than complicated."
+reflection = "Complexity births beauty, complication births confusion."
+reflectiion2 = " This quote reminds me how the complex thing give life meaningful purpose compilated things make life frustrating"
+
+print(phrase)
+print(reflection)
+print(reflectiion2)

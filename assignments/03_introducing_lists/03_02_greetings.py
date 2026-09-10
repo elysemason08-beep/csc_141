da73f09e7420aa60da909b5bdd9c066cf02760e5
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-10 14:38:53 -0400

    friend messages

--- assignments/03_introducing_lists/03_02_greetings.py
+++ assignments/03_introducing_lists/03_02_greetings.py
@@ -5,3 +5,9 @@ print(names[0])
 print(names[1])
 print(names[2])
 print(names[3])
+
+message = [' I love my friend!', ' My friend is smart!',' My friend is the best!',' My friends is kind!']
+print(message[0] +"" + names[0])
+print(message[1] +"" + names[1])
+print(message[2] +"" + names[2])
+print(message[3] +"" + names[3])

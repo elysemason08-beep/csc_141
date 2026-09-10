c275c246c3322a3f51b40d6ce213be39a1b927aa
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-10 14:30:26 -0400

    sync hello world

--- assignments/01_getting_started/01_02_hello_world_typos.py
+++ assignments/01_getting_started/01_02_hello_world_typos.py
@@ -1,8 +1,8 @@
 '''
 Print this sucks!
 I have no real prior coding experience.
-Learning coding is very hard for me, but I one day want to become a Python master.
+Learning coding is very hard for me, but I want to become a Python master.
 I NEED THE POWER OF THE PHYTHON.
-I thought I had the power of python; I guess not with C!
+I thought I had the power of Python; I guess not with C!
 Hello world! 
 '''

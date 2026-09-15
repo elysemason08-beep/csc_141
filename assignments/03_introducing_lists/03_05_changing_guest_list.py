8b0eda4f1e2dcc24a3e057fec10e018c24541179
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-15 14:01:50 -0400

    Guest changing list

---
+++ assignments/03_introducing_lists/03_05_changing_guest_list.py
@@ -0,0 +1,13 @@
+''''''
+guests = ['Miracle', 'Peyton', 'Amber']
+
+for guest in guests:
+    if guest == 'Amber':
+        print("Amber, you seem so genuine as my newest friend. You get French fries. Happy return aboard!")
+    elif guest == 'Miracle':
+        print("Miracle, you are like my sister. Therefore, you get a diamond-level badge; here's your lobster mac and cheese. Welcome aboard!")
+    elif guest == 'Peyton':
+        print("Peyton, you are my sister! Step or not, you earned the diamond-level badge; here's your lobster and shrimp; you're already aboard!")
+print("Sad news: Amber cannot make it to dinner tonight; she's sick.")
+guests[2] = 'Keke'
+print(f"Keke, welcome to dinner! You get French fries. Welcome aboard!")

6d79b3ff45792dfeafc4505772175e660ff824af
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-15 18:31:01 -0400

    this took to long treats

--- assignments/03_introducing_lists/03_10_every_function.py
+++ assignments/03_introducing_lists/03_10_every_function.py
@@ -1,1 +1,26 @@
 ''''''
+desserts = [" cake", " cookies", " ice cream", " Chocolate", "candy", " brownies"]
+print(desserts)
+desserts.append(" cake")
+print(desserts)
+desserts.insert(1, " chocolate")
+print(desserts)
+
+del desserts[2]
+print(desserts)
+desserts.pop()
+print(desserts)
+
+desserts.remove("candy")
+print(desserts)
+print(sorted(desserts))
+print(desserts)
+desserts.reverse()
+
+print(desserts)
+print(len(desserts))
+desserts.sort()
+print(desserts)
+
+desserts.sort(reverse=True)
+print(desserts)

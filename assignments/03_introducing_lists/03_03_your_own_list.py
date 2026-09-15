d4a7d9f23a14a74d2cf0aa6ee0d79686c3279ae7
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-15 13:41:16 -0400

    pink cars for a star

---
+++ assignments/03_introducing_lists/03_03_your_own_list.py
@@ -0,0 +1,14 @@
+''''''
+message = "My favorite transportation method is a car."
+print(message)
+Message = " My future dream car would include pink 2026 bentley "
+print(Message)
+transportation = ['pink suv', 'pink taxi', 'white sedan', 'pink coupe', ' pink convertible', 'light pink limousine', 'light pink ferrari','white toyota camery']
+print(f"I would like to own a {transportation[0]}.")
+print(f"I would like to own a {transportation[1]}.")
+print(f"I would like to own a {transportation[2]}.")
+print(f"I would like to own a {transportation[3]}.")
+print(f"I would like to own a {transportation[4]}.")
+print(f"I would like to own a {transportation[5]}.")
+print(f"I would like to own a {transportation[6]}.")
+print(f"I would like to own a {transportation[7]}.")

7f5537f83daa32fd35546e7f84d05420edc0f75f
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-17 14:13:25 -0400

    million in a range

---
+++ assignments/04_working_with_lists/04_05_summing_a_million.py
@@ -0,0 +1,10 @@
+''''''
+numbers = list(range(1, 1000001))
+for number in numbers:
+    print(number)
+    
+numbers = list(range(1, 1000001))
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))

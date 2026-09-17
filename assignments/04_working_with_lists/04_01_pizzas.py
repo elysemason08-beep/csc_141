53ba547da08291ca01088ef8aa450ae3d0d6ba92
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-17 13:41:14 -0400

    pizza w a loop

---
+++ assignments/04_working_with_lists/04_01_pizzas.py
@@ -0,0 +1,9 @@
+''''''
+pizza = ['pepperoni', 'bacon', 'cheese', 'sausage']
+
+for pizza in pizza:
+    print(f"I love {pizza} pizza.")
+
+print("But Bacon pizza is my favorite out of all of them!")
+print("I really love pizza and could eat it almost every day!")
+print("Pizza is one of my favorite foods!")

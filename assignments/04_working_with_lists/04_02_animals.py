0e26db2579df80b6730f960164ee65bdf2a7ab1e
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-17 13:53:38 -0400

    animal babies

---
+++ assignments/04_working_with_lists/04_02_animals.py
@@ -0,0 +1,7 @@
+''''''
+animals = ['tiger', 'leopard', 'cat', 'cheetah', 'lion', 'jaguar']
+print("The all the names in the list of animals are:")
+for animal in animals:
+    print(f" the great {animal}")
+for animal in animals:
+    print(f" the great {animal} has baby-like hunter features! They love to hunt male and female prey, but they love being babied as well.")

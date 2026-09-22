adad906b8805b04d5ccff068ee6b2d16f321f1ee
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-22 13:47:04 -0400

    message

---
+++ assignments/04_working_with_lists/04_14_pep8.py
@@ -0,0 +1,6 @@
+''''''
+# 4-14. PEP 8
+# I looked through the PEP 8 style guide.
+# PEP 8 explains how to format Python code so it is
+# clean, readable, and consistent.
+# I love how they call an python inventor authors!

87d02a1269bd5ed620e33c298e46f17e54d1f3f3
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-22 14:04:03 -0400

    python and number production

--- assignments/04_working_with_lists/04_06_odd_numbers.py
+++ assignments/04_working_with_lists/04_06_odd_numbers.py
@@ -3,3 +3,6 @@ odd_numbers = list(range(1, 20, 2))
 
 for number in odd_numbers:
     print(number)
+message = ' I wonder if python can be used for art thats made up of deciamls in the nearby future for early education.'
+print = message
+# Just a random thought with number production and children.

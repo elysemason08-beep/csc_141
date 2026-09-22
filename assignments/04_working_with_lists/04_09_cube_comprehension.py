bc2eef4d43c86a569881bf626de9757f9a478d41
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-22 14:05:54 -0400

    youtube

--- assignments/04_working_with_lists/04_09_cube_comprehension.py
+++ assignments/04_working_with_lists/04_09_cube_comprehension.py
@@ -2,3 +2,6 @@
 cubes = [number ** 3 for number in range(1, 11)]
 
 print(cubes)
+# My favorite part is running with through the terminal and seeing how fast the range goes
+# It's something small but makes me love python, when you finally get done, especially a code that took a while.
+# You hit new terminal and the play button like you would on youtube, and your work becomes alive.

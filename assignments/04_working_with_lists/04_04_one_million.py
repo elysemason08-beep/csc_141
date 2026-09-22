c1629667f961b46fa81a5b58cbd09989e7a1d984
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-22 14:01:49 -0400

    python scientific power

--- assignments/04_working_with_lists/04_04_one_million.py
+++ assignments/04_working_with_lists/04_04_one_million.py
@@ -2,3 +2,8 @@
 numbers = list(range(1, 1000001))
 for number in numbers:
     print(number)
+# these interest me because, python is used for scientists to conduct better experiemnts with vacines and dana use-age
+message = ' Scientist have the python power! They use python for the fast variety of key tiny infragment information, saving money and lives!'
+print = message 
+message = ' if we can count to a million in python, we can save a million lives with it as well!'
+print = message

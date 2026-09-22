00269152f522651ee3134f81e845186619ea35c5
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-22 14:09:31 -0400

    power

--- assignments/04_working_with_lists/04_08_cube_numbers.py
+++ assignments/04_working_with_lists/04_08_cube_numbers.py
@@ -2,3 +2,6 @@
 cubes = list(range(1, 10))
 for number in cubes:
     print(number ** 3)
+    # I wonder if I if the number can go over a million
+    # just a python power thought
+    # how powerful can i just about become'!!!!!!!

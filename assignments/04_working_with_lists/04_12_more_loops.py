f0976cced789e5fc5546bef23e306666e1c5a84f
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-22 13:27:47 -0400

    food loops

---
+++ assignments/04_working_with_lists/04_12_more_loops.py
@@ -0,0 +1,14 @@
+pizzas = ['pepperoni', 'bacon', 'cheese', 'sausage']
+
+friend_pizzas = pizzas.copy()
+
+pizzas.append('mushroom')
+friend_pizzas.append('pineapple')
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
---
+++ assignments/04_working_with_lists/04_13_buffet.py
@@ -0,0 +1,10 @@
+''''''
+foods = ("chicken", "noodles", "pork", "sauces", "vegetables")
+
+for food in foods:
+    print(food)
+
+foods = ("chicken", "rice", "pork", "soup", "vegetables")
+
+for food in foods:
+    print(food)

59faa54d9127b9a6447d56b0c975f6437c66c719
elysemason08-beep (elysemason08-beep) <elysemason08@gmail>
2026-09-24 14:04:43 -0400

    list

---
+++ assignments/05_if_statements/ 05_01_conditional_tests.py
@@ -0,0 +1,39 @@
+''''''
+cake = "chocolate"
+cereal = "Frosted Flakes"
+seafood = "shrimp"
+barbeque = "chicken"
+pants = "jeans"
+shirts = "t-shirts"
+jackets = "hoodies"
+numbers = [1, 2, 3, 4, 5]
+
+print("Is cake == 'chocolate'? I predict True.")
+print(cake == "chocolate")
+
+print("\nIs cereal == 'Frosted Flakes'? I predict True.")
+print(cereal == "Frosted Flakes")
+
+print("\nIs seafood == 'shrimp'? I predict True.")
+print(seafood == "shrimp")
+
+print("\nIs barbeque == 'chicken'? I predict True.")
+print(barbeque == "chicken")
+
+print("\nIs pants == 'jeans'? I predict True.")
+print(pants == "jeans")
+
+print("\nIs cake == 'vanilla'? I predict False.")
+print(cake == "vanilla")
+
+print("\nIs cereal == 'Cheerios'? I predict False.")
+print(cereal == "Cheerios")
+
+print("\nIs seafood == 'salmon'? I predict False.")
+print(seafood == "salmon")
+
+print("\nIs jackets == 'leather'? I predict False.")
+print(jackets == "leather")
+
+print("\nIs 6 in numbers? I predict False.")
+print(6 in numbers)
